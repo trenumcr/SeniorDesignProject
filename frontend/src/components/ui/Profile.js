db4d9b6024169805a9ad77bc8f87cb9a2a26fe0e
Thomas Klein <Thomasjklein3@gmail.com>
2020-11-18 22:05:52 -0500

    Initial user profile grid design

--- frontend/src/components/ui/Profile.js
+++ frontend/src/components/ui/Profile.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-export default function Profile() {
-  return(
-    <div>
-      This is the profile page
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function CenteredGrid() {
+  const classes = useStyles();
+
+  // Paper class pads the contents
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <Paper className={classes.paper}>Profile Picture</Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper className={classes.paper}>Name, School, etc. in a list format</Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>Contact Me Button</Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}><h1>Bio</h1><p1>Information on my projects and field of focus</p1></Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}><h1>Posts Made</h1>Component 1, Component 2, Component 3</Paper>
+        </Grid>
+      </Grid>
     </div>
-  )
+  );
 }
